d280d73b3879c8232dc80d8c7a93968284dad055
ranjith <ranjith.r1k98@gmail.com>
2024-10-18 19:41:36 +0530

    updated deleted button

--- data/cart.js
+++ data/cart.js
@@ -1,4 +1,34 @@
-export const cart = [];
+export let cart = [
+    {
+        id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
+        image: "images/products/athletic-cotton-socks-6-pairs.jpg",
+        name: "Black and Gray Athletic Cotton Socks - 6 Pairs",
+        rating: {
+          stars: 4.5,
+          count: 87
+        },
+        priceCents: 1090,
+        keywords: [
+          "socks",
+          "sports",
+          "apparel"
+        ]
+      },
+      {
+        id: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
+        image: "images/products/intermediate-composite-basketball.jpg",
+        name: "Intermediate Size Basketball",
+        rating: {
+          stars: 4,
+          count: 127
+        },
+        priceCents: 2095,
+        keywords: [
+          "sports",
+          "basketballs"
+        ]
+      }
+];
 
 export function addToCart(productId) {
     let matchingitem;
@@ -18,4 +48,15 @@ export function addToCart(productId) {
             })
         }
         console.log(cart);
+}
+
+export function removeFromCart(productId) {
+    let updatedCart = [];
+    cart.forEach((cartitem) => {
+        if(productId !== cartitem.id){
+            updatedCart.push(cartitem);
+        }
+    });
+    cart = updatedCart;
+    console.log(cart);
 }
--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,54 +1,7 @@
 import {products} from "../data/products.js";
-/* import {cart,addToCart} from "../data/cart.js"; */
 import { formatCurrency } from "./utils/money.js";
-let cart = [
-  {
-    id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
-    image: "images/products/athletic-cotton-socks-6-pairs.jpg",
-    name: "Black and Gray Athletic Cotton Socks - 6 Pairs",
-    rating: {
-      stars: 4.5,
-      count: 87
-    },
-    priceCents: 1090,
-    keywords: [
-      "socks",
-      "sports",
-      "apparel"
-    ]
-  },
-  {
-    id: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
-    image: "images/products/intermediate-composite-basketball.jpg",
-    name: "Intermediate Size Basketball",
-    rating: {
-      stars: 4,
-      count: 127
-    },
-    priceCents: 2095,
-    keywords: [
-      "sports",
-      "basketballs"
-    ]
-  },
-  {
-    id: "83d4ca15-0f35-48f5-b7a3-1ea210004f2e",
-    image: "images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg",
-    name: "Adults Plain Cotton T-Shirt - 2 Pack",
-    rating: {
-      stars: 4.5,
-      count: 56
-    },
-    priceCents: 799,
-    keywords: [
-      "tshirts",
-      "apparel",
-      "mens"
-    ],
-    type: "clothing",
-    sizeChartLink: "images/clothing-size-chart.png"
-  }
-];
+import { cart,removeFromCart } from "../data/cart.js";
+
 let carthtml ='';
 let i= 0;
 cart.forEach((cartItem) => {
@@ -59,7 +12,8 @@ cart.forEach((cartItem) => {
             matchingProduct = productItem
         }
     })   
-    carthtml += `<div class="cart-item-container">
+    carthtml += `<div class="cart-item-container 
+        js-cart-item-container-${matchingProduct.id}">
             <div class="delivery-date">
               Delivery date: Tuesday, June 21
             </div>
@@ -82,7 +36,7 @@ cart.forEach((cartItem) => {
                   <span class="update-quantity-link link-primary">
                     Update
                   </span>
-                  <span class="delete-quantity-link link-primary">
+                  <span class="delete-quantity-link link-primary js-delete-link" data-product-id ="${matchingProduct.id}">
                     Delete
                   </span>
                 </div>
@@ -138,3 +92,13 @@ cart.forEach((cartItem) => {
 })
 
 document.querySelector('.js-cart-section').innerHTML = carthtml;
+
+document.querySelectorAll('.js-delete-link').forEach((link)=>{
+link.addEventListener('click',()=>{
+  const productId = link.dataset.productId;
+  removeFromCart(productId);
+
+  document.querySelector(`.js-cart-item-container-${productId}`).remove();
+  
+})
+})
