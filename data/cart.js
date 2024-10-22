6e11b9d2d5d7735aa23d3d9cea8939db5a76bf0d
ranjith <ranjith.r1k98@gmail.com>
2024-10-22 11:39:14 +0530

    create automated tests for addToCart

--- data/cart.js
+++ data/cart.js
@@ -1,15 +1,19 @@
-export let cart = JSON.parse(localStorage.getItem('cart'));
-if(!cart) {
-    cart = [{
-        id: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
-        quantity: 1,
-        deliveryOptions: '2'
-    },{
-        id: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
-        quantity: 1,
-        deliveryOptions: '3'
-    }]
+export let cart; 
+loadFromStorage();
 
+export function loadFromStorage() {
+   cart = JSON.parse(localStorage.getItem('cart'));
+    if(!cart) {
+        cart = [{
+            id: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
+            quantity: 1,
+            deliveryOptions: '2'
+        },{
+            id: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
+            quantity: 1,
+            deliveryOptions: '3'
+        }]
+    }
 }
 
 function saveToStorage() {
